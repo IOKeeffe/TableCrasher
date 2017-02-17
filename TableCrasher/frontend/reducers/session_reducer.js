e3c5db593c8f90af41c412c13341fcd3bab5a60e
Ian <Iokeef@gmail.com>
2017-02-17 12:19:16 -0500

    Fix signin and signup pages, no errors.

--- TableCrasher/frontend/reducers/session_reducer.js
+++ TableCrasher/frontend/reducers/session_reducer.js
@@ -1,7 +1,7 @@
-import { RECEIVE_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_ERRORS, RECEIVE_CURRENT_USER, SIGNING_IN, SIGNING_UP } from '../actions/session_actions';
 import merge from 'lodash/merge';
 
-const SessionReducer = (oldState = {currentUser: null, errors: []}, action) => {
+const SessionReducer = (oldState = {currentUser: null, signingIn: false, signingUp: false, errors: []}, action) => {
   Object.freeze(oldState);
   const newState = merge({}, oldState);
 
@@ -9,6 +9,19 @@ const SessionReducer = (oldState = {currentUser: null, errors: []}, action) => {
   case RECEIVE_CURRENT_USER:
     newState.errors = [];
     newState.currentUser = action.user;
+
+    return newState;
+
+  case SIGNING_IN:
+    newState.errors = [];
+    newState.signingIn = action.signingIn;
+    newState.signingUp = false;
+    return newState;
+
+  case SIGNING_UP:
+    newState.errors = [];
+    newState.signingIn=false;
+    newState.signingUp = action.signingUp;
     return newState;
 
   case RECEIVE_ERRORS:
