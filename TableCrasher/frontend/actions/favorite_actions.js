980b213e5c5141452de69339e70ff6f740135197
Ian <Iokeef@gmail.com>
2017-02-22 10:21:08 -0500

    Add redux cycle for favorites.

---
+++ TableCrasher/frontend/actions/favorite_actions.js
@@ -0,0 +1,38 @@
+import * as FavoriteApiUtil from '../util/favorite_api_util';
+
+export const RECEIVE_FAVORITE = 'RECEIVE_FAVORITE';
+export const RECEIVE_FAVORITES = 'RECEIVE_FAVORITES';
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
+
+export const fetchFavorite = id => dispatch => {
+  return FavoriteApiUtil.fetchFavoriteRestaurants(id).then(favorite => {
+    dispatch(receiveFavorite(favorite));
+  });
+};
+
+export const fetchFavorites = () => dispatch => {
+  return FavoriteApiUtil.fetchFavorites().then(favorites => dispatch(receiveFavorites(favorites)));
+};
+
+export const deleteFavorite = id => dispatch => {
+  return FavoriteApiUtil.deleteFavorite(id).then(favorites => dispatch(removeFavorite(favorites)));
+};
+
+export const createFavorite = favorite => dispatch => {
+  return FavoriteApiUtil.createFavorite(favorite).then(favorite => dispatch(receiveFavorite(favorite)));
+};
+
+export const receiveFavorite = (favorite) => {
+  return {type: RECEIVE_FAVORITE,
+    favorite};
+};
+
+export const receiveFavorites = (favorites) => ({
+  type: RECEIVE_FAVORITES,
+  favorites,
+});
+
+export const removeFavorite = (favorite) => ({
+  type: REMOVE_FAVORITE,
+  favorite
+});
---
+++ TableCrasher/frontend/reducers/favorites_reducer.js
@@ -0,0 +1,20 @@
+import merge from 'lodash/merge';
+import {RECEIVE_FAVORITE, RECEIVE_FAVORITES, REMOVE_FAVORITE } from '../actions/favorite_actions';
+
+export default (oldState = {favorites: []}, action) => {
+  Object.freeze(oldState);
+  let newState = merge({}, oldState);
+  switch(action.type) {
+  case RECEIVE_FAVORITE:
+    newState.favorites.push(action.favorite);
+    return newState;
+  case RECEIVE_FAVORITES:
+    newState.favorites = action.favorites;
+    return newState;
+  case REMOVE_FAVORITE:
+    delete(newState[action.favorite.id]);
+    return newState;
+  default:
+    return oldState;
+  }
+};
